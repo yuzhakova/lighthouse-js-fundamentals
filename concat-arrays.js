91ba9a8a9701396796ab4a2be5b495c094e8682e
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-06-12 01:46:41 -0700

    Add files via upload

---
+++ concat-arrays.js
@@ -0,0 +1,9 @@
+function concat(theArray1, theArray2) {
+  let theArray3 = [];
+  theArray3 = theArray1.concat(theArray2);
+  return theArray3;
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
